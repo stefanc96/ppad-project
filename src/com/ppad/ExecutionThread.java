bfd5dab0d8229ee18426d25b6a89e6d7522e8968
stefan <stefanchelaru96@gmail.com>
2021-01-10 11:03:20 +0200

    +final version

--- src/com/ppad/ExecutionThread.java
+++ src/com/ppad/ExecutionThread.java
@@ -5,25 +5,27 @@ import java.util.Queue;
 public class ExecutionThread extends Thread {
     private final Boolean execute;
     private final Queue<Runnable> runnables;
+    private final int keepAliveTime;
 
-    public ExecutionThread(String name, Boolean execute, Queue<Runnable> runnables) {
+
+    public ExecutionThread(String name, Boolean execute, Queue<Runnable> runnables, int keepAliveTime) {
         super(name);
         this.execute = execute;
         this.runnables = runnables;
+        this.keepAliveTime = keepAliveTime;
     }
 
     @Override
     public void run() {
         try {
-            // Continue to execute when the execute flag is true, or when there are runnables in the queue
             while (execute || !runnables.isEmpty()) {
                 Runnable runnable;
-                // Poll a runnable from the queue and execute it
+
                 while ((runnable = runnables.poll()) != null) {
                     runnable.run();
                 }
-                // Sleep in case there wasn't any runnable in the queue. This helps to avoid hogging the CPU.
-                Thread.sleep(1);
+
+                Thread.sleep(keepAliveTime);
             }
         } catch (RuntimeException | InterruptedException e) {
             throw new ExecutionThreadPoolException(e);
--- src/com/ppad/Main.java
+++ src/com/ppad/Main.java
@@ -1,12 +1,10 @@
 package com.ppad;
 
-import java.util.concurrent.Executors;
-
 public class Main {
     public static void main(String[] args) {
-        java.util.concurrent.ThreadPoolExecutor executor = (java.util.concurrent.ThreadPoolExecutor) Executors.newFixedThreadPool(2);
-        ThreadPoolExecutor threadPoolExecutor = ThreadPoolExecutor.newFixedThreadPool(2);
+        ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(10, 20, 1, 10);
 
+        threadPoolExecutor.addWorker();
         for (int i = 1; i <= 20; i++) {
             Task task = new Task("Task " + i);
             System.out.println("Created : " + task.getName());
