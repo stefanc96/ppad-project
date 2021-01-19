e9c177a1aac794b331fdc1fbe294100492a28f27
stefan <stefanchelaru96@gmail.com>
2021-01-19 17:59:07 +0200

    +updated sleep time

--- src/com/ppad/Task.java
+++ src/com/ppad/Task.java
@@ -14,7 +14,7 @@ public class Task implements Runnable {
     public void run() {
         System.out.println(Thread.currentThread().getName() + " executing : " + name);
         try {
-            Thread.sleep(2000);
+            Thread.sleep(500);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
